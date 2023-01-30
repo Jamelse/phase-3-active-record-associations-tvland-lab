00a63f1ef4290e6d5aa5d2b088a940a217e25f1c
Jacob <jacobamelse@hotmail.com>
2023-01-29 22:13:08 -0600

    commit tree a6fa288c3dc5d03738f89a7deb3391737543441a

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,9 @@
 class Show < ActiveRecord::Base
-  
+  belongs_to :network
+  has_many :characters
+  has_many :actors, through: :characters
+
+  def actors_list
+    self.actors.map {|char| "#{char.first_name} #{char.last_name}"}
+  end
 end
